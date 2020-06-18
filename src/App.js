75eeacf0dae8af3228581ada0012e48c6e92777e
Michael Almpertis <mike.almpertis@gmail.com>
2020-06-19 00:57:24 +0300

    added logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.png';
 import './App.css';
+import topImage from './assets/images/topwave.png'
+import bottomImage from './assets/images/bottomwaves.png'
+import googlePlayBadge from './assets/images/google-play-badge.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img style={{
+          width: '100%',
+          maxHeight: 150,
+          position: 'absolute',
+          top: 0
+        }} src={topImage} alt="top wave"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Triply. Coming soon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={googlePlayBadge} className="store-badge" alt="google play badge" />
+        </div>
+        <img style={{
+          width: '100%',
+          maxHeight: 150,
+          position: 'absolute',
+          bottom: 0
+        }} src={bottomImage} alt="top wave"/>
       </header>
     </div>
   );
